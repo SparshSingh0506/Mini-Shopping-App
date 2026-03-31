36a735ab9cd13c2c98f3c2f3d2637b2cf8e1edc0
SparshSingh0506 <sparshsingh562006@gmail.com>
2026-04-01 00:45:06 +0530

    add productCard component & api interfaces

---
+++ components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import { Product } from '../interfaces/Product'
+
+interface productCardProps {
+    product: Product
+}
+
+export const ProductCard = ({ product }: productCardProps) => {
+    return (
+        <div>
+
+        </div>
+    );
+}
