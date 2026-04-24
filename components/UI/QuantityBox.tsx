3106e128215ca282d015634d8a0f55a3cd3b749c
SparshSingh0506 <sparshsingh562006@gmail.com>
2026-04-25 00:56:26 +0530

    fix spacing

--- components/UI/QuantityBox.tsx
+++ components/UI/QuantityBox.tsx
@@ -2,21 +2,21 @@ import { useCartContext } from '../../contexts/CartContext';
 import type { CartItem } from '../../contexts/CartContext';
 import type { Product } from '../../interfaces/Product';
 
-export const QuantityBox = ({ product, itemInCart }: {product: Product, itemInCart: CartItem}) => {
+export const QuantityBox = ({ product, itemInCart }: { product: Product, itemInCart: CartItem }) => {
   const { id } = product;
   const { addToCart, removeFromCart } = useCartContext();
 
   return (
 
     <div className="flex items-center border-2 border-gray-800 
-              rounded-lg overflow-hidden bg-gray-50">
+      rounded-lg overflow-hidden bg-gray-50">
 
       {/* Minus */}
       <button
         onClick={() => removeFromCart(id)}
         className="px-3 py-1 text-lg font-bold 
-                 bg-gray-200 hover:bg-red-300 hover:cursor-pointer
-                  active:scale-95 active:rounded-sm transition"
+        bg-gray-200 hover:bg-red-300 hover:cursor-pointer
+        active:scale-95 active:rounded-sm transition"
       >
         -
       </button>
@@ -29,12 +29,12 @@ export const QuantityBox = ({ product, itemInCart }: {product: Product, itemInCa
       {/* Plus */}
       <button
         onClick={() => {
-          const {quantity, ...product} = itemInCart
+          const { quantity, ...product } = itemInCart
           addToCart(product)
         }}
         className="px-3 py-1 text-lg font-bold 
-                 bg-gray-200 hover:bg-green-300 hover:cursor-pointer
-                  active:scale-95 active:rounded-sm transition"
+        bg-gray-200 hover:bg-green-300 hover:cursor-pointer
+        active:scale-95 active:rounded-sm transition"
       >
         +
       </button>
